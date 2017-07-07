6b3c8f66e67cbe8f6b1b685987ceb70d86accc05
Akram Shehadi <akram.shehadi@gmail.com>
2017-07-07 02:16:39 -0500

    Fix minor require error

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var mAuthVerifier	= require("mauth_verifier");
-var mAuthMint		= require("mauth_mint");
+var mAuthVerifier	= require("./mauth_verifier");
+var mAuthMint		= require("./mauth_mint");
 
 module.exports = {
     mAuthVerifier : mAuthVerifier,
